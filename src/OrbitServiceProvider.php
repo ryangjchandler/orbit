8345d21426cb01430eeecd6e56a1827d2d42d960
Ryan Chandler <ryangjchandler@gmail.com>
2021-03-20 17:38:45 +0000

    wip [skip-ci]

---
+++ src/OrbitServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Orbit;
+
+use Illuminate\Support\ServiceProvider;
+
+class OrbitServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
