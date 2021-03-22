746c4da6ea15d70a54211af57abe70f45e284832
Ryan Chandler <ryangjchandler@gmail.com>
2021-03-22 14:20:45 +0000

    tests: custom driver

--- tests/AdvancedOrbitalTest.php
+++ tests/AdvancedOrbitalTest.php
@@ -3,12 +3,14 @@
 namespace Orbit\Tests;
 
 use Orbit\Tests\Fixtures\CustomKey;
+use Orbit\Tests\Fixtures\JsonModel;
 
 class AdvancedOrbitalTest extends TestCase
 {
     public function tearDown(): void
     {
         CustomKey::all()->each->delete();
+        JsonModel::all()->each->delete();
     }
 
     public function test_it_can_create_files_using_custom_primary_key()
@@ -34,5 +36,14 @@ class AdvancedOrbitalTest extends TestCase
 
         $this->assertFileDoesNotExist(__DIR__.'/content/customkeys/Ryan.md');
         $this->assertFileExists(__DIR__.'/content/customkeys/'.$custom->name.'.md');
+    }
+
+    public function test_it_can_use_a_custom_driver()
+    {
+        $json = JsonModel::create([
+            'name' => 'Ryan',
+        ]);
+
+        $this->assertFileExists(__DIR__.'/content/jsonmodels/'.$json->getKey().'.json');
     }
 }
