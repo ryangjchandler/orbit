31cc24968412f85900b45b99af3324dd28788541
Ryan Chandler <ryangjchandler@gmail.com>
2021-03-22 14:11:51 +0000

    tests: restoration

--- tests/SoftDeletesTest.php
+++ tests/SoftDeletesTest.php
@@ -39,4 +39,25 @@ class SoftDeletesTest extends TestCase
 
         $this->assertFileDoesNotExist(__DIR__.'/content/softdeletedposts/'.$post->id.'.md');
     }
+
+    public function test_it_will_remove_deleted_at_when_restoring_file()
+    {
+        $post = SoftDeletedPost::create([
+            'title' => 'Example',
+        ]);
+
+        $post->delete();
+
+        $this->assertNotEmpty($post->deleted_at);
+
+        $file = file_get_contents(__DIR__.'/content/softdeletedposts/'.$post->id.'.md');
+
+        $this->assertStringContainsString(sprintf('deleted_at: \'%s\'', $post->deleted_at), $file);
+
+        $post->restore();
+
+        $file = file_get_contents(__DIR__.'/content/softdeletedposts/'.$post->id.'.md');
+
+        $this->assertStringNotContainsString('deleted_at', $file);
+    }
 }
