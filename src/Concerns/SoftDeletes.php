3cbb5978c3748a7436a2ba6dab0b2bc3e473521e
Ryan Chandler <ryangjchandler@gmail.com>
2021-03-22 11:05:00 +0000

    feature: finish soft deletes support

--- src/Concerns/SoftDeletes.php
+++ src/Concerns/SoftDeletes.php
@@ -2,13 +2,47 @@
 
 namespace Orbit\Concerns;
 
-use Illuminate\Database\Eloquent\SoftDeletes as EloquentSoftDeletes;
+use Orbit\Facades\Orbit;
+use Orbit\Events\OrbitalDeleted;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\ColumnDefinition;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Database\Eloquent\SoftDeletes as EloquentSoftDeletes;
 
 trait SoftDeletes
 {
     use EloquentSoftDeletes;
+
+    public static function bootSoftDeletes()
+    {
+        static::addGlobalScope(new SoftDeletingScope);
+
+        static::deleted(function (Model $model) {
+            $status = Orbit::driver(static::getOrbitalDriver())->save(
+                $model, static::getOrbitalPath()
+            );
+
+            OrbitalDeleted::dispatch($model);
+
+            return $status;
+        });
+
+        static::forceDeleted(function (Model $model) {
+            if ($model->callTraitMethod('shouldDelete', $model) === false) {
+                return;
+            }
+
+            $status = Orbit::driver(static::getOrbitalDriver())->delete(
+                $model,
+                static::getOrbitalPath()
+            );
+
+            OrbitalDeleted::dispatch($model);
+
+            return $status;
+        });
+    }
 
     public function schemaSoftDeletes(Blueprint $table)
     {
@@ -21,5 +55,10 @@ trait SoftDeletes
         }
 
         $table->softDeletes($this->getDeletedAtColumn());
+    }
+
+    public function shouldDeleteSoftDeletes()
+    {
+        return $this->isForceDeleting();
     }
 }
