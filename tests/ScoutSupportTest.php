19adb3f60f3af349b4902c1279f61d8d05a035c8
Ryan Chandler <ryangjchandler@gmail.com>
2022-03-17 13:33:04 +0000

    tests: scout support

---
+++ tests/ScoutSupportTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Orbit\Tests;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Blueprint;
+use Laravel\Scout\Scout;
+use Laravel\Scout\ScoutServiceProvider;
+use Laravel\Scout\Searchable;
+use Orbit\Concerns\Orbital;
+use Orbit\Tests\Fixtures\Post;
+
+class ScoutSupportTest extends TestCase
+{
+    protected function tearDown(): void
+    {
+        ScoutPost::all()->each->delete();
+    }
+
+    /** @test */
+    public function orbit_models_can_be_searched_with_scout()
+    {
+        config()->set('scout.driver', 'collection');
+
+        ScoutPost::create([
+            'title' => 'Scout post one',
+            'content' => 'Post one',
+        ]);
+
+        ScoutPost::create([
+            'title' => 'Scout post two',
+            'content' => 'Post two',
+        ]);
+
+        $this->assertCount(2, ScoutPost::search('Post')->get());
+        $this->assertCount(1, ScoutPost::search('post two')->get());
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return array_merge([
+            ScoutServiceProvider::class,
+        ], parent::getPackageProviders($app));
+    }
+}
+
+class ScoutPost extends Model
+{
+    use Orbital;
+    use Searchable;
+
+    protected $guarded = [];
+
+    public static function schema(Blueprint $table)
+    {
+        $table->id('id');
+        $table->string('title');
+        $table->string('content');
+    }
+}
