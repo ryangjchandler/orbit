e300011ce9cd976f5b60e699de456ebd8f4a25ea
Ryan Chandler <ryangjchandler@gmail.com>
2022-04-17 23:22:53 +0100

    feature: soft deletes

---
+++ src/Observers/SoftDeletesObserver.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Orbit\Observers;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SoftDeletesObserver
+{
+    public function restored(Model $model)
+    {
+        (new OrbitalObserver)->updated($model);
+    }
+
+    public function deleted(Model $model)
+    {
+        (new OrbitalObserver)->updated($model);
+    }
+
+    public function forceDeleted(Model $model)
+    {
+        (new OrbitalObserver)->deleted($model);
+    }
+}
