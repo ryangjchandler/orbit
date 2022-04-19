9baeb18a7e3b056d5bc73db1448aec517602ca88
Ryan Chandler <ryangjchandler@gmail.com>
2022-04-19 15:27:35 +0100

    wip

--- tests/SoftDeletes/Test.php
+++ tests/SoftDeletes/Test.php
@@ -10,12 +10,14 @@ test('soft deletes > update the file correctly', function () {
         'title' => 'foobar',
     ]);
 
-    assertFileExists(__DIR__ . '/content/1.md');
+    $path = __DIR__ . "/content/{$model->getKey()}.md";
+
+    assertFileExists($path);
 
     $model->delete();
 
-    assertFileExists(__DIR__ . '/content/1.md');
-    assertFileContains(__DIR__ . '/content/1.md', 'deleted_at: ' . $model->deleted_at->toIso8601String());
+    assertFileExists($path);
+    assertFileContains($path, 'deleted_at: ' . $model->deleted_at->toIso8601String());
 });
 
 afterEach(function () {
