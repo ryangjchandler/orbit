344c8b642c993248c92420b0ba2401fb573f1908
Daniel Weaver <godismyjudge95@gmail.com>
2023-03-06 18:01:29 -0500

    Adding flat json driver to config

--- config/orbit.php
+++ config/orbit.php
@@ -7,6 +7,7 @@ return [
     'drivers' => [
         'md' => \Orbit\Drivers\Markdown::class,
         'json' => \Orbit\Drivers\Json::class,
+        'flat_json' => \Orbit\Drivers\FlatJson::class,
         'yaml' => \Orbit\Drivers\Yaml::class,
     ],
 
