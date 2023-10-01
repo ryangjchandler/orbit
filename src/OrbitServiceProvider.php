e6d956a0e967d71c859a6ee058f3ff5ce910ec5f
Ryan Chandler <ryangjchandler@gmail.com>
2023-10-02 00:25:11 +0100

    wip

---
+++ src/OrbitServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Orbit;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class OrbitServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('orbit');
+    }
+}
