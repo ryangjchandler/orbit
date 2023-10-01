7fb1623bf6284aa9a73ce782382ad78439c1e788
Ryan Chandler <ryangjchandler@gmail.com>
2023-10-02 00:50:59 +0100

    wip

--- src/Contracts/Orbital.php
+++ src/Contracts/Orbital.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Schema\Blueprint;
 
 interface Orbital
 {
+    /**
+     * Define the structure of your Orbital model.
+     */
     public static function schema(Blueprint $table): void;
+
+    /**
+     * Declare which driver the Orbital should use.
+     *
+     * @return class-string<\Orbit\Contracts\Driver>
+     */
+    public function getOrbitDriver(): string;
 }
